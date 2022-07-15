cb767831a86b5ebcc7db8305907ad6b2768b81ea
Nada Ahmed <102693671+NaDaWahballa@users.noreply.github.com>
2022-07-15 23:38:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,291 @@
+<?php
+
+session_start();
+?>
+<html>
+    
+    <head>
+        <title>Bones Diseases</title>
+        <link rel="stylesheet" href="ho.css">
+        <link rel="stylesheet" href="bla3.html">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <style>
+             button{
+                
+                 margin: 30px;
+                 padding: 15px;
+                 font-family: comic sans ms;
+                 color:#CC5500 ;
+                 border-radius: 10%;
+                 font-size: large;
+            
+        } 
+  
+        /*creating animation effect*/ 
+        button:hover { 
+            color: #111; 
+            background: 	#ED9121; 
+            box-shadow: 0 0 50px 	#08E8DE , 0 0 40px 	#08E8DE ,0 0 80px 	#08E8DE ;
+             
+        } 
+        </style>
+    </head>
+    
+    <body>
+        <nav>
+            <ul>
+                <li><a class="aa" href="#1" >HOME</a></li>
+                <li><a class="aa" href="#2">DISEASES</a></li>
+                <li><a class="aa" href="#3">ABOUT_US</a></li>
+                <li><a class="aa" href="#4">OUR_TEAM</a></li>
+                
+            </ul>
+        </nav>
+        <div  id="1" style="height: 100%; width: 100% ; background-image: linear-gradient(rgba(0,0,0,0.8),rgba(0,0,0,0.8)), url(wh.jpeg) ;background-size: cover;background-position: center; display: inline-block">
+            <H1>HOME</H1>
+            <h1 style="font-size: xx-large ; text-align: center ; color: #CC5500; font-family: comic sans ms ">-----Bones Diseases-----</h1>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6" >
+                        <div class="ab" style="font-size: x-large;font-family: comic sans ms;color: #FFF5EE; margin: 12px ; ">
+                            Since you entered our site, this means that you suffer from a bone disease,<br>
+                            or one of your relatives suffers from a bone disease .... Or you want to get<br>
+                            a general overview of what bone diseases are.<br>
+                            If you suffer from a bone disease and want to treat it using some exercises,<br>
+                            log on to our site to follow the exercises with us step by step. <br>
+                        </div>
+                        <a href="in.php"> <button  >TO LOG IN >></button></a>
+                        <div style="font-size: x-large;font-family: comic sans ms;color: #FFF5EE; margin: 12px"> And if you want to get a general overview about bone diseases...</div>
+                    
+                        <!--
+                      <a href="#2"> <button  > OVERVIEW </button></a>
+                      -->
+                    </div>
+                    <div class="col-md-6">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <DIV id="2" style="height: 100%; width: 100% ; background: #CC5500; display: inline-block ">
+            <h1>DISEASES</h1>
+            <h2 style="color:#033249 ; font-size: xx-large ;margin: 12px">DISEASES</h2>
+            <div class="ab" style="font-size: x-large;font-family: comic sans ms;color: #FFF5EE;margin: 12px">
+                Our site contains a lot of bone diseases that we will show it to you ....
+            </div>
+            
+            <div class="diseases">
+          <table width="100%">
+            <tr>
+                <td width="25%">
+                    <img src="aa2.jpeg" style="width: 300px;height: 300px"><br>
+                    <a href="bla2.html" style=" text-decoration: none ; font-weight: bold ;color: #033249;font-size: xx-large;margin-left: 30px">Osteoporosis</a>
+                </td> <br><br>
+        
+        <td width="25%">
+            <img src="aa.jpeg" style="width: 300px;height: 300px"><br>
+            <a href="pro.html" style=" text-decoration: none ; font-weight: bold ;color: #033249 ;font-size: xx-large;margin-left: 90px">Arthritis</a><br><br>
+        </td>
+        <td width="25%">
+            <img src="aa1.jpeg" style="width: 300px;height: 300px"><br>
+            <a href="bla3.html" style=" text-decoration: none ; font-weight: bold ;color: #033249 ;font-size: xx-large ;margin-left: 125px">Gout</a><br><br>
+        </td>
+        
+        <td width="25%">
+            <img src="aa3.jpeg" style="width: 300px;height: 300px ;margin-left: 30px"><br>
+            <a href="bla4.html" style=" text-decoration: none ; font-weight: bold ;color: #033249 ;font-size: xx-large ;">Palindromic_Rheumatism</a><br><br>
+         </td>
+            </tr>
+            <tr>
+                 <td width="25%">
+                    <img src="cc0.png" style="width: 300px;height: 300px ;margin-left: 30px ; margin-top: 20px" ><br>
+                    <a href="bla.html" style=" text-decoration: none ; font-weight: bold ;color: #033249 ;font-size: xx-large ; margin-left: 40px">Muscle_Atrophy</a><br><br>
+                 </td>
+                 <td width="25%">
+                    <img src="ww0.jpg" style="width: 300px;height: 300px ;margin-left: 30px"><br>
+                    <a href="bla5.html" style=" text-decoration: none ; font-weight: bold ;color: #033249 ;font-size: xx-large ; margin-left: 60px">Torn_Meniscus </a><br><br>
+                 </td>
+            </tr>
+          </table>
+            </div>
+            
+            
+        </DIV>
+        <div id="3" style="height: 100%; width: 100% ; background: #CC5500; display: inline-block">
+            <h1>ABOUT US</h1>
+            <h2 style="color:#033249 ; font-size: xx-large ;margin: 12px" >About Us</h2>
+             <div  style="font-size: x-large;font-family: comic sans ms;color: #FFF5EE; margin: 12px">
+                Our site contains many designed pages, and these pages contain pictures<br>
+                and paragraphs, as well as pages with videos that show the user some<br>
+                exercises and also design personal pages, including colors and writings.<br> 
+            </div>
+             
+             <div class="col-md-6" style="margin: 12px">
+                 <p style="font-size: x-large;color: #FFF5EE"> - Web Design</p>
+                <div class="progress">
+                    <div class="progress-bar b0" style="width: 90%"><span>90%</span></div>
+                </div>
+                <p style="font-size: x-large;color: #FFF5EE"> - Adobe Photoshop</p>
+                <div class="progress">
+                    <div class="progress-bar b1" style="width: 80%"><span>80%</span></div>
+                </div>
+                <p style="font-size: x-large;color: #FFF5EE"> - UI Design</p>
+                <div class="progress">
+                    <div class="progress-bar b2" style="width: 85%"><span>85%</span></div>
+                </div>
+                <p style="font-size: x-large;color: #FFF5EE"> - UX Design</p>
+                <div class="progress">
+                    <div class="progress-bar b3" style="width: 75%"><span>75%</span></div>
+                </div>
+            </div>
+            
+            <!--
+            
+            <div class="border"></div>
+            <div class="about-section-row">
+                <div class="about-section-col">
+                    <div class="about">
+                        <p>
+                            Our site contains many designed pages, and these pages contain pictures
+                            and paragraphs, as well as pages with videos that show the user some
+                            exercises and also design personal pages, including colors and writings.
+                        </p>
+                    </div>
+                </div>
+                <div class="about-section-col">
+                    <div class="skills">
+                        <div class="skill">
+                            <div class="title">Web Design</div>
+                            <div class="progress">
+                                <div class="progress-bar p1">90%</div>
+                            </div>
+                        </div>
+                        
+                        <div class="skill">
+                            <div class="title">UI Design</div>
+                            <div class="progress">
+                                <div class="progress-bar p2">80%</div>
+                            </div>
+                        </div>
+                        
+                        <div class="skill">
+                            <div class="title">UX Design</div>
+                            <div class="progress">
+                                <div class="progress-bar p3">50%</div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    
+                </div>
+            </div>
+            
+            -->
+            
+            <!--
+             <h2 style="color:#033249 ; font-size: xx-large ;margin: 12px" >---------Our Team---------</h2>
+             
+             
+           <div style="font-size: x-large;font-family: comic sans ms;color: #FFF5EE; margin: 12px">
+            Our team contains three people, these are the people who worked hard<br>
+            to get this website and this design, and these people are ...<br> 
+           </div>
+            
+            
+            <div class="row">
+                <div class="col-md-3 profile-pic text-center">
+                   
+                        <img src="nada.jpeg" class="img-responsive" style="margin-left: 18px ;width: 300px;height: 300px">
+                  
+                    
+                    
+                        <img src="rania.jpeg" class="img-responsive" style="margin-left: 18px; width: 300px;height: 300px">
+                    
+                    
+                   
+                        <img src="aya.jpeg" class="img-responsive" style="margin-left: 18px ;width: 300px;height: 300px">
+                        
+                        <img src="nada2.jpeg" class="img-responsive" style="margin-left: 18px ;width: 300px;height: 300px">
+                   
+                    
+                    
+                    
+                </div>
+            </div>
+            -->
+        </div>
+        
+        
+        
+         <DIV id="4" style="height: 100%; width: 100% ; background: #CC5500; display: inline-block ">
+            <h1>OUR TEAM</h1>
+            <h2 style="color:#033249 ; font-size: xx-large ;margin: 12px" >---------Our Team---------</h2>
+            <div style="font-size: x-large;font-family: comic sans ms;color: #FFF5EE; margin: 12px">
+            Our team contains three people, these are the people who worked hard<br>
+            to get this website and this design, and these people are ...<br> <br>
+           </div>
+            
+           <div class="row">
+                <div class="col-md-3 profile-pic text-center">
+                   
+                   <table width="50%">
+                    <tr>
+                        <td >
+                        
+                            <img src="mlj.jpeg" class="img-responsive" style="margin-left: 18px ;width: 300px;height: 300px"><br>
+                            <h3 style="color: #033249 ;font-size: x-large ;margin-left: 80px">Nada Ahmed</h3>
+                            <ul style="margin-left: 40px">
+                                <a href="#"><li><i class="fa fa-facebook"></i></li></a>
+                                <a href="#"><li><i class="fa fa-twitter"></i></li></a>
+                                <a href="#"><li><i class="fa fa-linkedin"></i></li></a>
+                                <a href="#"><li><i class="fa fa-Instagram"></i></li></a>
+                            </ul>
+                    
+                        </td>
+                        
+                        <td>
+                            <img src="kj.jpeg" class="img-responsive" style=" ; width: 300px;height: 300px"><br>
+                            <h3 style="color: #033249 ;font-size: x-large ; margin-left: 80px">Rania Ahmed</h3>
+                            <ul class="ay">
+                                <a href="#"><li><i class="fa fa-facebook"></i></li></a>
+                                <a href="#"><li><i class="fa fa-twitter"></i></li></a>
+                                <a href="#"><li><i class="fa fa-linkedin"></i></li></a>
+                                <a href="#"><li><i class="fa fa-Instagram"></i></li></a>
+                            </ul>
+                        </td>
+                        
+                    
+                        <td>
+                             <img src="jk.jpg" class="img-responsive" style=" ;width: 300px;height: 300px"><br>
+                             <h3 style="color: #033249 ;font-size: x-large ; margin-left: 80px">Aya Ahmed</h3>
+                             <ul class="ay">
+                                <a href="#"><li><i class="fa fa-facebook"></i></li></a>
+                                <a href="#"><li><i class="fa fa-twitter"></i></li></a>
+                                <a href="#"><li><i class="fa fa-linkedin"></i></li></a>
+                                <a href="#"><li><i class="fa fa-Instagram"></i></li></a>
+                            </ul> 
+                           
+                         </td>
+                        
+                     </tr>   
+                   </table>
+                    
+                    
+                    
+                </div>
+            </div> 
+           
+        
+            
+            
+        </DIV>
+             
+             
+        
+    </body>
+</html>
+
